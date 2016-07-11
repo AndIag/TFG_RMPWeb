3ab11ee55b1a3ce446142fac95970d470c310b27
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-07-11 18:50:19 +0200

    Splited i18n config to other file

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('RestMaPla', [   'ui.bootstrap',
+                                'ui.router',
+                                'ngCookies',
+                                'pascalprecht.translate',// angular-translate
+                                'tmh.dynamicLocale'// angular-dynamic-locale
+                            ])
+
+angular.module('RestMaPla').constant('LOCALES', {
+    'locales': {
+        'en_GB': 'English',
+        'es_ES': 'Español'
+    },
+    'preferredLocale': 'en_GB'
+});
