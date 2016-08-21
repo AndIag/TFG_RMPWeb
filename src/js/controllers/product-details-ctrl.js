1168fcf84ddbbce02e450de2cf1672dc37d9d2ec
Iagocanalejas <iagocanalejas@gmail.com>
2016-08-21 11:43:56 +0200

    Solved breadcrumb bug with long words

--- src/js/controllers/product-details-ctrl.js
+++ src/js/controllers/product-details-ctrl.js
@@ -10,7 +10,6 @@ myApp.controller('ProductDetailsCtrl', ['$scope', '$state', '$stateParams', '$tr
             if(($scope.product == null) && $stateParams.productId){
                 ProductService.getProductById($stateParams.productId).success(function(data){
                     $scope.product = JSON.parse(JSON.stringify(data));
-                    console.log(data);
                     BreadcrumbManager.changePage($scope.product.name);
                 }).error(function(data){
                     Flash.create('danger', $translate.instant('error.loading.products'), 3000);
--- src/js/factories/breadcrumb-factory.js
+++ src/js/factories/breadcrumb-factory.js
@@ -4,11 +4,13 @@ myApp.factory('BreadcrumbManager', [
         return {
             data: {
                 uri: '',
-                page: ''
+                page: '',
+                shortPage: ''
             },
             changePage: function(p) {
                 this.data.page = p;
                 this.data.uri = staticUri + p;
+                this.data.shortPage = (p.length > 16) ? p.substring(0,13) + '...' : p;
             }
         };
     }
