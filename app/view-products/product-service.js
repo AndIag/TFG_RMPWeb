95b3119b27a415ac597d6b68618dc0ceee2d0736
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-11-07 17:23:19 +0100

    fixed product ui-sref + added icons for simple/pack product

---
+++ app/view-products/product-service.js
@@ -0,0 +1,7 @@
+angular.module('RestMaPla.product.service', [])
+    .service('ProductService', ['CrudService', '$http', '$q', function (CrudService, $http, $q) {
+        var ENDPOINT = 'http://localhost:9090/restmapla';
+        return {
+
+        }
+    }]);
