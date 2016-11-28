a33f6d9c9d2119f2c7be0a7b52425a90e07c7cd4
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-11-28 23:43:16 +0100

    added typeahead to add products form

--- app/view-products/product-ctrl.js
+++ app/view-products/product-ctrl.js
@@ -8,11 +8,11 @@ angular.module('RestMaPla.products.controller', ['ngFlash', 'ngDialog', 'RestMaP
 
             $scope.init = function () {
                 BreadCrumbService.setBreadCrumb($translate.instant('views.index.products'));
-                getBrandsPage(1);
+                getProductsPage(1);
             };
 
             $scope.changePage = function (newPage, oldPage) {
-                getBrandsPage(newPage);
+                getProductsPage(newPage);
             };
 
             $scope.searchByName = function () {
@@ -27,27 +27,58 @@ angular.module('RestMaPla.products.controller', ['ngFlash', 'ngDialog', 'RestMaP
 
             $scope.showCreate = function () {
                 $scope.product = {};
+                if (!CrudService.response.hasOwnProperty("categories")) loadCategories();
+                if (!CrudService.response.hasOwnProperty("brands")) loadBrands();
                 dialog = ngDialog.open({template: 'view-products/add-form.html', scope: $scope, controller: this});
             };
 
-            $scope.removeBrand = function (product) {
+            $scope.removeProduct = function (product) {
                 CrudService.removeItem(CrudService.endpoints.PRODUCTS_ENDPOINT, product.id).success(function (data) {
                     CrudService.response.products.items = CrudService.response.products.items.filter(function (e) {
                         return e.id !== product.id;
                     });
-                    CrudService.response.products.count = CrudService.response.products.count -1;
+                    CrudService.response.products.count = CrudService.response.products.count - 1;
                 }).error(function (data) {
                     Flash.clear();
                     Flash.create('danger', $translate.instant('error.removing'), 3000);
                 });
             };
 
-            function getBrandsPage(page) {
+            function getProductsPage(page) {
                 PaginationService.data.currentPage = page;
                 CrudService.getPaginatedItems(CrudService.endpoints.PRODUCTS_ENDPOINT, (page - 1),
                     PaginationService.data.itemsPerPage).success(function (data) {
 
                     CrudService.response.products = JSON.parse(JSON.stringify(data));
+                }).error(function (data) {
+                    Flash.clear();
+                    Flash.create('danger', $translate.instant('error.loading'), 3000);
+                });
+            }
+
+            // USED IN ADD PRODUCT DIALOG
+
+            $scope.searchBrand = function () {
+                CrudService.findItemsByName(CrudService.endpoints.BRANDS_ENDPOINT, $scope.product.brand).success(function (data) {
+                    CrudService.response.brands = JSON.parse(JSON.stringify(data));
+                }).error(function (data) {
+                    Flash.clear();
+                    Flash.create('danger', $translate.instant('error.loading'), 3000);
+                });
+            };
+
+            function loadCategories() {
+                CrudService.getItems(CrudService.endpoints.CATEGORIES_ENDPOINT).success(function (data) {
+                    CrudService.response.categories = JSON.parse(JSON.stringify(data));
+                }).error(function (data) {
+                    Flash.clear();
+                    Flash.create('danger', $translate.instant('error.loading'), 3000);
+                });
+            }
+
+            function loadBrands() {
+                CrudService.getItems(CrudService.endpoints.BRANDS_ENDPOINT).success(function (data) {
+                    CrudService.response.brands = JSON.parse(JSON.stringify(data));
                 }).error(function (data) {
                     Flash.clear();
                     Flash.create('danger', $translate.instant('error.loading'), 3000);
